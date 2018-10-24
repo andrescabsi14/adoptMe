7e47d07f5797c92f0315983cc63de524c06ca6f5
Andres Cabrera <andrescabsi@gmail.com>
2018-10-24 15:46:35 -0500

    Search form, ESLint some rules disabled.

---
+++ src/SearchParams.js
@@ -0,0 +1,111 @@
+import React from "react";
+import { ANIMALS } from "petfinder-client";
+import pf from "petfinder-client";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class SearchParams extends React.Component {
+  state = {
+    location: "Ann Arbor, MI",
+    animal: "",
+    breed: "",
+    breeds: []
+  };
+  handleLocationChange = () => {
+    this.setState({
+      location: event.target.value
+    });
+  };
+  handleAnimalChange = () => {
+    this.setState(
+      {
+        animal: event.target.value,
+        breed: ""
+      },
+      this.getBreeds
+    );
+  };
+  handleBreedChange = () => {
+    this.setState({
+      breed: event.target.value
+    });
+  };
+  getBreeds() {
+    if (this.state.animal) {
+      petfinder.breed.list({ animal: this.state.animal }).then(data => {
+        if (
+          data.petfinder &&
+          data.petfinder.breeds &&
+          Array.isArray(data.petfinder.breeds.breed)
+        ) {
+          this.setState({
+            breeds: data.petfinder.breeds.breed
+          });
+        } else {
+          this.setState({
+            breeds: []
+          });
+        }
+      });
+    } else {
+      this.setState({
+        breeds: []
+      });
+    }
+  }
+  render() {
+    return (
+      <div className="search-params">
+        <label htmlFor="location">
+          Location
+          <input
+            onChange={this.handleLocationChange}
+            id="location"
+            value={this.state.location}
+            placeholder="Location"
+          />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={this.state.animal}
+            onChange={this.handleAnimalChange}
+            onBlur={this.handleAnimalChange}
+          >
+            {ANIMALS.map(animal => (
+              <option key={animal} value={animal}>
+                {animal}
+              </option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            disabled={this.state.breeds === 0}
+            id="breed"
+            value={this.state.breed}
+            onChange={this.handleBreedChange}
+            onBlur={this.handleBreedChange}
+          >
+            {/*  Blank Option */}
+            <option />
+
+            {this.state.breeds.map(breed => (
+              <option key={breed} value={breed}>
+                {breed}
+              </option>
+            ))}
+          </select>
+        </label>
+        <button>Submit</button>
+      </div>
+    );
+  }
+}
+
+export default SearchParams;
