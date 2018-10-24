3527c3db31b1fd909820ad42088c2a00d6f206f2
Andres Cabrera <andrescabsi@gmail.com>
2018-10-24 12:53:02 -0500

    API call, test API response in component's state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,53 @@
 import React from "react";
 import { render } from "react-dom";
+import pf from "petfinder-client";
 import Pet from "./Pet";
 
+// Remove key access
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({
+        output: "full",
+        location: "Ann Arbor, MI"
+      })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets
+        });
+      });
+  }
   render() {
     return (
       <React.Fragment>
         <h1>Adopt Me!</h1>
-        <Pet name="Nicky" animal="Dog" breed="Mixed" />
+        <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre>
+        {/* <Pet name="Nicky" animal="Dog" breed="Mixed" /> */}
       </React.Fragment>
     );
   }
